0c20c79fd3aff1c05ffa59ef223847ae1fcd6b37
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2022-12-06 13:00:33 +0530

    Formatted

--- frontend/micro-ui/web/src/App.js
+++ frontend/micro-ui/web/src/App.js
@@ -1,42 +1,32 @@
 import React from "react";
 
-
 import { initDSSComponents } from "@egovernments/digit-ui-module-dss";
 import { DigitUI } from "@egovernments/digit-ui-module-core";
 import { initLibraries } from "@egovernments/digit-ui-libraries";
 import { initEngagementComponents } from "@egovernments/digit-ui-module-engagement";
-import { initFSMLibraries } from '@egovernments/digit-ui-fsm-libraries';
+import { initFSMLibraries } from "@egovernments/digit-ui-fsm-libraries";
 
-import { initFSMComponents } from '@egovernments/digit-ui-module-fsm';
-import {   initHRMSComponents } from "@egovernments/digit-ui-module-hrms";
-window.contextPath=window?.globalConfigs?.getConfig("CONTEXT_PATH");
+import { initFSMComponents } from "@egovernments/digit-ui-module-fsm";
+import { initHRMSComponents } from "@egovernments/digit-ui-module-hrms";
+window.contextPath = window?.globalConfigs?.getConfig("CONTEXT_PATH");
 
 initLibraries();
 initFSMLibraries();
 
-const enabledModules = [
-  "FSM",
-  "Payment",
-  "DSS",
-  "Engagement",
-  "HRMS",
-];
-window.Digit.ComponentRegistryService.setupRegistry({
- 
-});
+const enabledModules = ["FSM", "Payment", "DSS", "Engagement", "HRMS"];
+window.Digit.ComponentRegistryService.setupRegistry({});
 
 initDSSComponents();
 initEngagementComponents();
 initFSMComponents();
 initHRMSComponents();
 
-
 const moduleReducers = (initData) => ({
-  initData
+  initData,
 });
 
 function App() {
-  window.contextPath=window?.globalConfigs?.getConfig("CONTEXT_PATH");
+  window.contextPath = window?.globalConfigs?.getConfig("CONTEXT_PATH");
   const stateCode =
     window.globalConfigs?.getConfig("STATE_LEVEL_TENANT_ID") ||
     process.env.REACT_APP_STATE_LEVEL_TENANT_ID;
