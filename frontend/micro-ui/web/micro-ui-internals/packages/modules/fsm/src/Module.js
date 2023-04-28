bae79f3359e5dbefeb9161b89db45ed28a758e9a
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2023-04-28 15:45:24 +0530

    SM-2304,SM-2305,SM-2315: Fixes work in progress

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/Module.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/Module.js
@@ -84,7 +84,7 @@ const FSMLinks = ({ matchPath, userType }) => {
   const roleBasedLoginRoutes = [
     {
       role: "FSM_DSO",
-      from: "/digit-ui/citizen/fsm/dso-dashboard",
+      from: `/${window?.contextPath}/citizen/fsm/dso-dashboard`,
       dashoardLink: "CS_LINK_DSO_DASHBOARD",
       loginLink: "CS_LINK_LOGIN_DSO",
     },
@@ -117,38 +117,7 @@ const FSMLinks = ({ matchPath, userType }) => {
     });
 
     return <CitizenHomeCard header={t("CS_HOME_FSM_SERVICES")} links={links} Icon={CitizenTruck} />;
-  } else {
-    return (
-      <div className="employee-app-container">
-        <div className="ground-container">
-          <div className="employeeCard">
-            <div className="complaint-links-container">
-              <div className="header">
-                <span className="logo">
-                  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-                    <path d="M0 0h24v24H0z" fill="none"></path>
-                    <path
-                      d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-7 9h-2V5h2v6zm0 4h-2v-2h2v2z"
-                      fill="white"
-                    ></path>
-                  </svg>
-                </span>
-                <span className="text">{t("ES_TITLE_FSM")}</span>
-              </div>
-              <div className="body">
-                <span className="link">
-                  <Link to={`${matchPath}/inbox`}>{t("ES_TITLE_INBOX")}</Link>
-                </span>
-                <span className="link">
-                  <Link to={`${matchPath}/new-application/`}>{t("ES_TITLE_NEW_DESULDGING_APPLICATION")}</Link>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  } 
 };
 
 const componentsToRegister = {
