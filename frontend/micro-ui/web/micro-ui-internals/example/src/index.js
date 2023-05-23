3e1f0a7d7627134881ddb1694dda3ae1b8dedc35
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-05-23 17:32:50 +0530

    FSM CSS update

--- frontend/micro-ui/web/micro-ui-internals/example/src/index.js
+++ frontend/micro-ui/web/micro-ui-internals/example/src/index.js
@@ -16,7 +16,7 @@ import {
   HRMSModule,
   initHRMSComponents,
 } from '@egovernments/digit-ui-module-hrms';
-
+import "@egovernments/digit-ui-fsm-css/example/index.css";
 
 var Digit = window.Digit || {};
 
