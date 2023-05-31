5717bf802e87e469df8e3d04c026f7e4b88c279c
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2023-05-31 13:46:54 +0530

    SM-2441:Fixed css issue and missing total count, Addded verion by +1

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/pages/citizen/MyApplications/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/pages/citizen/MyApplications/index.js
@@ -1,17 +1,24 @@
-import React from "react";
-import { Header, Loader } from "@egovernments/digit-ui-react-components";
-import MyApplication from "./MyApplication";
-import { useTranslation } from "react-i18next";
+import React from 'react';
+import { Header, Loader } from '@egovernments/digit-ui-react-components';
+import MyApplication from './MyApplication';
+import { useTranslation } from 'react-i18next';
 
 export const MyApplications = () => {
   const { t } = useTranslation();
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const { info: userInfo } = Digit.UserService.getUser();
 
-  const { isLoading, isError, error, data: { data: { table: applicationsList } = {} } = {} } = Digit.Hooks.fsm.useSearchAll(tenantId, {
+  const {
+    isLoading,
+    isError,
+    error,
+    data: { data: { table: applicationsList } = {} } = {},
+  } = Digit.Hooks.fsm.useSearchAll(tenantId, {
     uuid: userInfo.uuid,
     limit: 100,
   });
+
+  const totalCounts = applicationsList?.length;
 
   if (isLoading) {
     return <Loader />;
@@ -19,14 +26,21 @@ export const MyApplications = () => {
 
   return (
     <React.Fragment>
+      <Header>
+        {t('CS_FSM_APPLICATION_TITLE_MY_APPLICATION')} ({totalCounts})
+      </Header>
       <div>
-        <Header>{t("CS_FSM_APPLICATION_TITLE_MY_APPLICATION")}</Header>
-        {applicationsList?.length > 0 &&
+        {totalCounts > 0 &&
           applicationsList.map((application, index) => (
             <div key={index}>
               <MyApplication application={application} />
             </div>
           ))}
+        {!totalCounts > 0 && (
+          <p style={{ marginLeft: '16px', marginTop: '16px' }}>
+            {t('CS_MYAPPLICATIONS_NO_APPLICATION')}
+          </p>
+        )}
       </div>
     </React.Fragment>
   );
