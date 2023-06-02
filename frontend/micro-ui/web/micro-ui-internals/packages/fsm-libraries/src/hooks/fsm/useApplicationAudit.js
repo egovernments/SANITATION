945891709865be6ba88a5320885bd928bbd68807
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-06-02 21:58:46 +0530

    Commit

--- frontend/micro-ui/web/micro-ui-internals/packages/fsm-libraries/src/hooks/fsm/useApplicationAudit.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/fsm-libraries/src/hooks/fsm/useApplicationAudit.js
@@ -4,6 +4,7 @@ import { useQuery, useQueryClient } from "react-query";
 const useApplicationAudit = (tenantId, filters) => {
   const client = useQueryClient();
   const query = useQuery(["FSM_APPLICATION_AUDIT", filters], () => FSMService.audit(tenantId, filters));
+  console.log("HEELO")
   return { ...query, revalidate: () => client.invalidateQueries(["FSM_APPLICATION_AUDIT", filters]) };
 };
 
