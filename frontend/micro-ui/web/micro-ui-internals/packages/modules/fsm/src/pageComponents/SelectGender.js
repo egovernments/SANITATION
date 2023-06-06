bb0ab6699ea53dfda1d7f6a4d0570bda62f29296
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2023-06-06 17:25:19 +0530

    SM-2444:Stepper clicakble fixes under progress, gender details changes added

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/pageComponents/SelectGender.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/pageComponents/SelectGender.js
@@ -1,17 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { Dropdown, FormStep, Loader, RadioOrSelect } from "@egovernments/digit-ui-react-components";
-import Timeline from "../components/TLTimelineInFSM";
+import React, { useState, useEffect } from 'react';
+import {
+  CardLabel,
+  Dropdown,
+  FormStep,
+  Loader,
+  RadioOrSelect,
+  CardText,
+  Row,
+} from '@egovernments/digit-ui-react-components';
+import Timeline from '../components/TLTimelineInFSM';
 
 const SelectGender = ({ config, onSelect, t, userType, formData }) => {
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const stateId = Digit.ULBService.getStateId();
-  const { data: GenderData, isLoading } = Digit.Hooks.fsm.useMDMS(stateId, "common-masters", "FSMGenderType");
+  const { data: GenderData, isLoading } = Digit.Hooks.fsm.useMDMS(
+    stateId,
+    'common-masters',
+    'FSMGenderType'
+  );
   const [genderType, setGenderType] = useState(formData?.genderType);
+  const userInfo = Digit.UserService.getUser().info;
+  console.log('++', userInfo);
 
   useEffect(() => {
     if (!isLoading && GenderData) {
       const preFilledGenderType = GenderData.filter(
-        (genderType) => genderType.code === (formData?.selectGender?.code || formData?.selectGender)
+        (genderType) =>
+          genderType.code ===
+          (formData?.selectGender?.code || formData?.selectGender)
       )[0];
       setGenderType(preFilledGenderType);
     }
@@ -19,9 +35,9 @@ const SelectGender = ({ config, onSelect, t, userType, formData }) => {
 
   const selectGenderType = (value) => {
     setGenderType(value);
-    if (userType === "employee") {
+    if (userType === 'employee') {
       onSelect(config.key, value);
-      onSelect("genderDetail", null);
+      onSelect('genderDetail', null);
     }
   };
 
@@ -32,21 +48,22 @@ const SelectGender = ({ config, onSelect, t, userType, formData }) => {
   const onSubmit = () => {
     onSelect(config.key, genderType);
   };
+  console.log('gender', config);
 
   if (isLoading) {
     return <Loader />;
   }
 
-  if (userType === "employee") {
+  if (userType === 'employee') {
     return (
       <div>
         <Dropdown
-          className="payment-form-text-input-correction"
+          className='payment-form-text-input-correction'
           isMandatory={config.isMandatory}
           selected={genderType}
           option={GenderData}
           select={selectGenderType}
-          optionKey="i18nKey"
+          optionKey='i18nKey'
           disable={config.disable}
           t={t}
         />
@@ -55,12 +72,33 @@ const SelectGender = ({ config, onSelect, t, userType, formData }) => {
   }
   return (
     <React.Fragment>
-      <Timeline currentStep={2} flow="APPLY" />
-      <FormStep config={config} onSelect={onSubmit} onSkip={onSkip} isDisabled={!genderType} t={t}>
+      <Timeline currentStep={2} flow='APPLY' />
+      <FormStep
+        config={config}
+        onSelect={onSubmit}
+        onSkip={onSkip}
+        isDisabled={!genderType}
+        t={t}
+      >
+        <div>
+          <Row
+            label={t('CS_NAME')}
+            text={t(userInfo.name)}
+            className='gender-label-row'
+          />
+          <Row
+            label={t('CORE_COMMON_PROFILE_MOBILE_NUMBER')}
+            text={t(userInfo.mobileNumber)}
+            className='gender-label-row'
+          />
+        </div>
+
+        <hr />
         <RadioOrSelect
+          name='gender'
           options={GenderData}
           selectedOption={genderType}
-          optionKey="i18nKey"
+          optionKey='i18nKey'
           onSelect={selectGenderType}
           t={t}
           isMandatory={config.isMandatory}
