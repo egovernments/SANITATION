7dfb78c8a2d8687520bba6e81dacb0f1467df7c8
nabeelmd-eGov <94039229+nabeelmd-eGov@users.noreply.github.com>
2023-08-17 12:04:04 +0530

    SM-2880: Added initial config for  Inbox/search Composer (#123)
    Co-authored-by: nabeelmd-eGov <nabeel.md@egovernments.com>

--- frontend/micro-ui/web/install-deps.sh
+++ frontend/micro-ui/web/install-deps.sh
@@ -4,6 +4,8 @@ BRANCH="$(git branch --show-current)"
 
 INTERNALS="micro-ui-internals"
 
+cp $INTERNALS/example/src/UICustomizations.js src/Customisations
+
 cd $INTERNALS && echo "installing packages" && yarn install && echo "starting build" && yarn build && echo "building finished" && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \; 
 cd ..
 
