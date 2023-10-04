746e537e8f8d71845e811230c97906bf04f9395d
SivajiGanesh Nangireddy <sivaji.ganesh@egovernments.org>
2023-10-04 11:46:00 +0530

    SM-3142: added audit tables and build configuration

--- pqm/src/main/resources/db/migration/main/V20230927123516__pqm_initial.sql
+++ pqm/src/main/resources/db/migration/main/V20230927123516__pqm_initial.sql
@@ -40,3 +40,44 @@ CREATE TABLE IF NOT EXISTS eg_pqm_test_result_documents
     CONSTRAINT fk_documents_pqm_test_results FOREIGN KEY (testId) REFERENCES eg_pqm_tests (id)
         ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS eg_pqm_tests_auditlog
+(
+    id                character varying(64),
+    tenantId          character varying(64),
+    plantId           character varying(255),
+    processId         character varying(255),
+    stageId           character varying(255),
+    materialId        character varying(255),
+    deviceId          character varying(255),
+    type              character varying(64),
+    status            character varying(64),
+    scheduledDate     bigint,
+    resultCriteria    json not null,
+    additionalDetails json,
+    isActive          boolean,
+    createdBy         character varying(64),
+    lastModifiedBy    character varying(64),
+    createdTime       bigint,
+    lastModifiedTime  bigint,
+    CONSTRAINT pk_pqm_tests PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS eg_pqm_test_result_documents_auditlog
+(
+    id               character varying(64),
+    testId           character varying(64),
+    documentUid      character varying(64),
+    documentUri      character varying(2084),
+    documentType     character varying(64),
+    filestoreId      character varying(250),
+    isActive         boolean,
+    createdBy        character varying(64),
+    lastModifiedBy   character varying(64),
+    createdTime      bigint,
+    lastModifiedTime bigint,
+    CONSTRAINT pk_pqm_test_result_documents PRIMARY KEY (id),
+    CONSTRAINT fk_documents_pqm_test_results FOREIGN KEY (testId) REFERENCES eg_pqm_tests (id)
+        ON DELETE CASCADE
+);
