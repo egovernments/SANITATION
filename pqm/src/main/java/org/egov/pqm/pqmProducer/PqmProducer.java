3f63efe6addc2034934d1e0816fa0a50456cc644
Priyanka-eGov <priyanka@egovernments.org>
2023-10-10 10:09:52 +0530

    SM-3147 producer changes

--- pqm/src/main/java/org/egov/pqm/pqmProducer/PqmProducer.java
+++ pqm/src/main/java/org/egov/pqm/pqmProducer/PqmProducer.java
@@ -2,7 +2,10 @@ package org.egov.pqm.pqmProducer;
 
 import org.egov.tracer.kafka.CustomKafkaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PqmProducer {
   @Autowired
   private CustomKafkaTemplate<String, Object> kafkaTemplate;
