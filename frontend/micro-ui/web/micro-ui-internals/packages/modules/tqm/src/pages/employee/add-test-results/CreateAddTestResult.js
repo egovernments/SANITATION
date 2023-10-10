1b0c8851f9c2d6b51ac60eee22c8e63892a4f761
Bhavya-egov <bhavya.mangal@egovernments.org>
2023-10-10 16:34:13 +0530

    made the different config file

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tqm/src/pages/employee/add-test-results/CreateAddTestResult.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tqm/src/pages/employee/add-test-results/CreateAddTestResult.js
@@ -2,156 +2,19 @@ import { Loader, FormComposerV2, Header, Toast } from "@egovernments/digit-ui-re
 import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
-
-
-export const newConfig = [
-  {
-    body: [
-      {
-        isMandatory: true,
-        key: "plant_name",
-        type: "radioordropdown",
-        label: "TQM_PLANT_NAME",
-        disable: false,
-        populators: {
-          name: "plant_name",
-          optionsKey: "name",
-          error: "ES_TQM_REQUIRED",
-          required: true,
-          mdmsConfig: {
-            masterName: "GenderType",
-            moduleName: "common-masters",
-            localePrefix: "COMMON_GENDER",
-          },
-        },
-      },
-      {
-        isMandatory: true,
-        key: "process",
-        type: "radioordropdown",
-        label: "TQM_TREATMENT_PROCESS",
-        disable: false,
-        populators: {
-          name: "process",
-          optionsKey: "name",
-          error: "ES_TQM_REQUIRED",
-          required: true,
-          mdmsConfig: {
-            masterName: "GenderType",
-            moduleName: "common-masters",
-            localePrefix: "COMMON_GENDER",
-          },
-        },
-      },
-      {
-        isMandatory: true,
-        key: "stage",
-        type: "radioordropdown",
-        label: "TQM_PROCESS_STAGE",
-        disable: false,
-        populators: {
-          name: "stage",
-          optionsKey: "name",
-          error: "ES_TQM_REQUIRED",
-          required: true,
-          mdmsConfig: {
-            masterName: "GenderType",
-            moduleName: "common-masters",
-            localePrefix: "COMMON_GENDER",
-          },
-        },
-      },
-      {
-        isMandatory: true,
-        key: "output",
-        type: "radioordropdown",
-        label: "TQM_OUTPUT_TYPE",
-        disable: false,
-        populators: {
-          name: "output",
-          optionsKey: "name",
-          error: "ES_TQM_REQUIRED",
-          required: true,
-          mdmsConfig: {
-            masterName: "GenderType",
-            moduleName: "common-masters",
-            localePrefix: "COMMON_GENDER",
-          },
-        },
-      }
-    ],
-  },
-  {
-    head: "ES_TQM_QUALITY_PARAMETERS",
-    body: [
-      {
-        label: "ES_TQM_TEST_PARAM_PH",
-        isMandatory: false,
-        key: "pH",
-        type: "number",
-        disable: false,
-        populators: { name: "qualityParameter.pH", error: "ES_TQM_TEST_PARAM_ERROR_MESSAGE", validation: { pattern: /^\d+$/ } },
-      },
-      {
-        label: "ES_TQM_TEST_PARAM_BOD",
-        isMandatory: false,
-        key: "BOD",
-        type: "number",
-        disable: false,
-        populators: { name: "qualityParameter.BOD", error: "ES_TQM_TEST_PARAM_ERROR_MESSAGE", validation: { pattern: /^\d+$/ } },
-      },
-      {
-        label: "ES_TQM_TEST_PARAM_COD",
-        isMandatory: false,
-        key: "COD",
-        type: "number",
-        disable: false,
-        populators: { name: "qualityParameter.COD", error: "ES_TQM_TEST_PARAM_ERROR_MESSAGE", validation: { pattern: /^\d+$/ } },
-      },
-      {
-        label: "ES_TQM_TEST_PARAM_TSS",
-        isMandatory: false,
-        key: "TSS",
-        type: "number",
-        disable: false,
-        populators: { name: "qualityParameter.TSS", error: "ES_TQM_TEST_PARAM_ERROR_MESSAGE", validation: { pattern: /^\d+$/ } },
-      },
-      {
-        label: "ES_TQM_TEST_PARAM_FCOLI",
-        isMandatory: false,
-        key: "F.Coli",
-        type: "number",
-        disable: false,
-        populators: { name: "qualityParameter.Coli", error: "ES_TQM_TEST_PARAM_ERROR_MESSAGE", validation: { pattern: /^\d+$/ } },
-      },
-      {
-        label: "ES_TQM_TEST_PARAM_ATTACH_DOCUMENTS",
-        "isMandatory": false,
-        "key": "basicDetails_photograph",
-        "type": "multiupload",
-        "preProcess": {
-          "convertStringToRegEx": ["populators.allowedFileTypes"]
-        },
-        "populators": {
-          "name": "basicDetails_photograph",
-          "allowedMaxSizeInMB": 2,
-          "maxFilesAllowed": 1,
-          "allowedFileTypes": /(jpg|jpeg|png|pdf)$/i,
-          "containerStyles": {
-            "marginBottom": "1rem"
-          }
-        }
-      }
-    ],
-  }
-];
+import { newConfig } from "./config";
 
 const Create = () => {
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const { t } = useTranslation();
   const history = useHistory();
   const [showToast, setShowToast] = useState(false);
-  console.log(showToast, "hee")
+
+  const closeToast = () => {
+      setTimeout(() => {
+        setShowToast(false);
+      }, 3000)
+  };
 
   const onSubmit = (data) => {
     console.log(data, "data");
@@ -162,22 +25,9 @@ const Create = () => {
         label: t('ES_TQM_ATLEAST_ONE_PARAMETER'),
         isError: true
       });
+      closeToast();
     }
   };
-
-  const closeToast = () => {
-    setShowToast(false);
-  };
-
-  //remove Toast after 3s
-  useEffect(() => {
-    if (showToast) {
-      setTimeout(() => {
-        closeToast();
-      }, 3000)
-    }
-  }, [showToast])
-
   const configs = newConfig;
 
   return (
@@ -198,7 +48,7 @@ const Create = () => {
         fieldStyle={{ marginRight: 0 }}
         noBreakLine ={true}
       />
-      {showToast && <Toast error={showToast.isError} label={showToast.label} isDleteBtn={true} onClose={closeToast} />}
+      {showToast && <Toast error={showToast.isError} label={showToast.label} isDleteBtn={true} onClose={()=>setShowToast(false)} />}
     </div>
   );
 };
---
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tqm/src/pages/employee/add-test-results/config.js
@@ -0,0 +1,141 @@
+export const newConfig = [
+    {
+      body: [
+        {
+          isMandatory: true,
+          key: "plant_name",
+          type: "radioordropdown",
+          label: "TQM_PLANT_NAME",
+          disable: false,
+          populators: {
+            name: "plant_name",
+            optionsKey: "name",
+            error: "ES_TQM_REQUIRED",
+            required: true,
+            mdmsConfig: {
+              masterName: "GenderType",
+              moduleName: "common-masters",
+              localePrefix: "COMMON_GENDER",
+            },
+          },
+        },
+        {
+          isMandatory: true,
+          key: "process",
+          type: "radioordropdown",
+          label: "TQM_TREATMENT_PROCESS",
+          disable: false,
+          populators: {
+            name: "process",
+            optionsKey: "name",
+            error: "ES_TQM_REQUIRED",
+            required: true,
+            mdmsConfig: {
+              masterName: "GenderType",
+              moduleName: "common-masters",
+              localePrefix: "COMMON_GENDER",
+            },
+          },
+        },
+        {
+          isMandatory: true,
+          key: "stage",
+          type: "radioordropdown",
+          label: "TQM_PROCESS_STAGE",
+          disable: false,
+          populators: {
+            name: "stage",
+            optionsKey: "name",
+            error: "ES_TQM_REQUIRED",
+            required: true,
+            mdmsConfig: {
+              masterName: "GenderType",
+              moduleName: "common-masters",
+              localePrefix: "COMMON_GENDER",
+            },
+          },
+        },
+        {
+          isMandatory: true,
+          key: "output",
+          type: "radioordropdown",
+          label: "TQM_OUTPUT_TYPE",
+          disable: false,
+          populators: {
+            name: "output",
+            optionsKey: "name",
+            error: "ES_TQM_REQUIRED",
+            required: true,
+            mdmsConfig: {
+              masterName: "GenderType",
+              moduleName: "common-masters",
+              localePrefix: "COMMON_GENDER",
+            },
+          },
+        }
+      ],
+    },
+    {
+      head: "ES_TQM_QUALITY_PARAMETERS",
+      body: [
+        {
+          label: "ES_TQM_TEST_PARAM_PH",
+          isMandatory: false,
+          key: "pH",
+          type: "number",
+          disable: false,
+          populators: { name: "qualityParameter.pH", error: "ES_TQM_TEST_PARAM_ERROR_MESSAGE", validation: { pattern: /^\d+$/ } },
+        },
+        {
+          label: "ES_TQM_TEST_PARAM_BOD",
+          isMandatory: false,
+          key: "BOD",
+          type: "number",
+          disable: false,
+          populators: { name: "qualityParameter.BOD", error: "ES_TQM_TEST_PARAM_ERROR_MESSAGE", validation: { pattern: /^\d+$/ } },
+        },
+        {
+          label: "ES_TQM_TEST_PARAM_COD",
+          isMandatory: false,
+          key: "COD",
+          type: "number",
+          disable: false,
+          populators: { name: "qualityParameter.COD", error: "ES_TQM_TEST_PARAM_ERROR_MESSAGE", validation: { pattern: /^\d+$/ } },
+        },
+        {
+          label: "ES_TQM_TEST_PARAM_TSS",
+          isMandatory: false,
+          key: "TSS",
+          type: "number",
+          disable: false,
+          populators: { name: "qualityParameter.TSS", error: "ES_TQM_TEST_PARAM_ERROR_MESSAGE", validation: { pattern: /^\d+$/ } },
+        },
+        {
+          label: "ES_TQM_TEST_PARAM_FCOLI",
+          isMandatory: false,
+          key: "F.Coli",
+          type: "number",
+          disable: false,
+          populators: { name: "qualityParameter.Coli", error: "ES_TQM_TEST_PARAM_ERROR_MESSAGE", validation: { pattern: /^\d+$/ } },
+        },
+        {
+          label: "ES_TQM_TEST_PARAM_ATTACH_DOCUMENTS",
+          "isMandatory": false,
+          "key": "basicDetails_photograph",
+          "type": "multiupload",
+          "preProcess": {
+            "convertStringToRegEx": ["populators.allowedFileTypes"]
+          },
+          "populators": {
+            "name": "basicDetails_photograph",
+            "allowedMaxSizeInMB": 2,
+            "maxFilesAllowed": 1,
+            "allowedFileTypes": /(jpg|jpeg|png|pdf)$/i,
+            "containerStyles": {
+              "marginBottom": "1rem"
+            }
+          }
+        }
+      ],
+    }
+  ];
