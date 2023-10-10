7d1970357aafcbbda51cd487fd0ea378e1c58371
admin1 <nitish@egovernments.org>
2023-10-10 10:27:23 +0530

    mdms validation

--- pqm/src/main/java/org/egov/pqm/util/Constants.java
+++ pqm/src/main/java/org/egov/pqm/util/Constants.java
@@ -4,7 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Constants {
-	
+
 	public static final String FSTPO_EMPLOYEE = "FSM_EMP_FSTPO";
+	public static final String MDMS_PLANT ="Plant";
+	public static final String MDMS_PROCESS ="Process";
+	public static final String MDMS_STAGE="Stage";
+	public static final String MDMS_MATERIAL ="Material";
+	public static final String PQM_JSONPATH_CODE ="PQM";
 
 }
