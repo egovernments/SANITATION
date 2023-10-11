9ceafc5c060c8ee422480d290d5eb22ef825dc36
Priyanka-eGov <priyanka@egovernments.org>
2023-10-11 11:28:36 +0530

    SM-3147 Adding consumes for create/update API

--- pqm/src/main/java/org/egov/pqm/web/controllers/PqmController.java
+++ pqm/src/main/java/org/egov/pqm/web/controllers/PqmController.java
@@ -29,7 +29,7 @@ public class PqmController {
 	@Autowired
 	private ResponseInfoFactory responseInfoFactory;
 
-  @PostMapping(value = "/_create")
+  @PostMapping(value = "/_create", consumes = {"application/json"})
   ResponseEntity<TestResponse> create(@Valid @RequestBody TestRequest testRequest) {
     Test test = pqmService.create(testRequest);
     List<Test> testList = new ArrayList<>();
@@ -40,7 +40,7 @@ public class PqmController {
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
-  @PostMapping(value = "/_update")
+  @PostMapping(value = "/_update", consumes = {"application/json"})
   ResponseEntity<TestResponse> update(@Valid @RequestBody TestRequest testRequest) {
     return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
   }
