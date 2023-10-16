41a54cc4bd2e12d9b260cf6cee56eb5587f0854e
Nipun Arora <aroranipun1@gmail.com>
2023-10-16 10:10:24 +0530

    breaking changes for new core version

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/utils/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/utils/index.js
@@ -1,16 +1,21 @@
-import BrowserUtil from "./browser";
-import * as date from "./date";
-import * as dss from "./dss";
-import * as locale from "./locale";
-import * as obps from "./obps";
-import * as pt from "./pt";
-import * as privacy from "./privacy";
-import PDFUtil, { downloadReceipt, downloadPDFFromLink, downloadBill, getFileUrl } from "./pdf";
-import getFileTypeFromFileStoreURL from "./fileType";
-import preProcessMDMSConfigInboxSearch from "./preProcessMDMSConfigInboxSearch";
+import BrowserUtil from './browser';
+import * as date from './date';
+import * as dss from './dss';
+import * as locale from './locale';
+import * as obps from './obps';
+import * as pt from './pt';
+import * as privacy from './privacy';
+import PDFUtil, {
+  downloadReceipt,
+  downloadPDFFromLink,
+  downloadBill,
+  getFileUrl,
+} from './pdf';
+import getFileTypeFromFileStoreURL from './fileType';
+import preProcessMDMSConfigInboxSearch from './preProcessMDMSConfigInboxSearch';
 
 const GetParamFromUrl = (key, fallback, search) => {
-  if (typeof window !== "undefined") {
+  if (typeof window !== 'undefined') {
     search = search || window.location.search;
     const params = new URLSearchParams(search);
     return params.has(key) ? params.get(key) : fallback;
@@ -24,67 +29,67 @@ const didEmployeeHasAtleastOneRole = (roles = []) => {
 
 const getPattern = (type) => {
   switch (type) {
-    case "Name":
+    case 'Name':
       return /^[^{0-9}^\$\"<>?\\\\~!@#$%^()+={}\[\]*,/_:;“”‘’]{1,50}$/i;
-    case "SearchOwnerName":
+    case 'SearchOwnerName':
       return /^[^{0-9}^\$\"<>?\\\\~!@#$%^()+={}\[\]*,/_:;“”‘’]{3,50}$/i;
-    case "MobileNo":
+    case 'MobileNo':
       return /^[6789][0-9]{9}$/i;
-    case "Amount":
+    case 'Amount':
       return /^[0-9]{0,8}$/i;
-    case "NonZeroAmount":
+    case 'NonZeroAmount':
       return /^[1-9][0-9]{0,7}$/i;
-    case "DecimalNumber":
+    case 'DecimalNumber':
       return /^\d{0,8}(\.\d{1,2})?$/i;
-    case "Email":
+    case 'Email':
       return /^(?=^.{1,64}$)((([^<>()\[\]\\.,;:\s$*@'"]+(\.[^<>()\[\]\\.,;:\s@'"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})))$/i;
-    case "Address":
+    case 'Address':
       return /^[^\$\"<>?\\\\~`!@$%^()+={}\[\]*:;“”‘’]{1,500}$/i;
-    case "PAN":
+    case 'PAN':
       return /^[A-Za-z]{5}\d{4}[A-Za-z]{1}$/i;
-    case "TradeName":
+    case 'TradeName':
       return /^[-@.\/#&+\w\s]*$/;
-    case "Date":
+    case 'Date':
       return /^[12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/i;
-    case "UOMValue":
+    case 'UOMValue':
       return /^(0)*[1-9][0-9]{0,5}$/i;
-    case "OperationalArea":
+    case 'OperationalArea':
       return /^(0)*[1-9][0-9]{0,6}$/i;
-    case "NoOfEmp":
+    case 'NoOfEmp':
       return /^(0)*[1-9][0-9]{0,6}$/i;
-    case "GSTNo":
+    case 'GSTNo':
       return /^\d{2}[A-Z]{5}\d{4}[A-Z]{1}\d[Z]{1}[A-Z\d]{1}$/i;
-    case "DoorHouseNo":
+    case 'DoorHouseNo':
       return /^[^\$\"'<>?~`!@$%^={}\[\]*:;“”‘’]{1,50}$/i;
-    case "BuildingStreet":
+    case 'BuildingStreet':
       return /^[^\$\"'<>?\\\\~`!@$%^()+={}\[\]*.:;“”‘’]{1,64}$/i;
-    case "Pincode":
+    case 'Pincode':
       return /^[1-9][0-9]{5}$/i;
-    case "Landline":
+    case 'Landline':
       return /^[0-9]{11}$/i;
-    case "PropertyID":
+    case 'PropertyID':
       return /^[a-zA-z0-9\s\\/\-]$/i;
-    case "ElectricityConnNo":
+    case 'ElectricityConnNo':
       return /^.{1,15}$/i;
-    case "DocumentNo":
+    case 'DocumentNo':
       return /^[0-9]{1,15}$/i;
-    case "eventName":
+    case 'eventName':
       return /^[^\$\"<>?\\\\~`!@#$%^()+={}\[\]*,.:;“”]{1,65}$/i;
-    case "eventDescription":
+    case 'eventDescription':
       return /^[^\$\"'<>?\\\\~`!@$%^()+={}\[\]*.:;“”‘’]{1,500}$/i;
-    case "cancelChallan":
+    case 'cancelChallan':
       return /^[^\$\"'<>?\\\\~`!@$%^()+={}\[\]*.:;“”‘’]{1,100}$/i;
-    case "FireNOCNo":
+    case 'FireNOCNo':
       return /^[a-zA-Z0-9-]*$/i;
-    case "consumerNo":
+    case 'consumerNo':
       return /^[a-zA-Z0-9/-]*$/i;
-    case "AadharNo":
+    case 'AadharNo':
       return /^([0-9]){12}$/;
-    case "ChequeNo":
+    case 'ChequeNo':
       return /^(?!0{6})[0-9]{6}$/;
-    case "Comments":
+    case 'Comments':
       return /^[^\$\"'<>?\\\\~`!@$%^()+={}\[\]*.:;“”‘’]{1,50}$/i;
-    case "OldLicenceNo":
+    case 'OldLicenceNo':
       return /^[a-zA-Z0-9-/]{0,64}$/;
   }
 };
@@ -94,23 +99,30 @@ const getUnique = (arr) => {
 };
 
 const getStaticMapUrl = (latitude, longitude) => {
-  const key = globalConfigs?.getConfig("GMAPS_API_KEY");
+  const key = globalConfigs?.getConfig('GMAPS_API_KEY');
   return `https://maps.googleapis.com/maps/api/staticmap?markers=${latitude},${longitude}&zoom=15&size=400x400&key=${key}&style=element:geometry%7Ccolor:0xf5f5f5&style=element:labels.icon%7Cvisibility:off&style=element:labels.text.fill%7Ccolor:0x616161&style=element:labels.text.stroke%7Ccolor:0xf5f5f5&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:poi%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:road%7Celement:geometry%7Ccolor:0xffffff&style=feature:road.arterial%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:road.highway%7Celement:geometry%7Ccolor:0xdadada&style=feature:road.highway%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:transit.line%7Celement:geometry%7Ccolor:0xe5e5e5&style=feature:transit.station%7Celement:geometry%7Ccolor:0xeeeeee&style=feature:water%7Celement:geometry%7Ccolor:0xc9c9c9&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x9e9e9e`;
 };
 
 const detectDsoRoute = (pathname) => {
-  const employeePages = ["search", "inbox", "dso-dashboard", "dso-application-details", "user", "Audit"];
+  const employeePages = [
+    'search',
+    'inbox',
+    'dso-dashboard',
+    'dso-application-details',
+    'user',
+    'Audit',
+  ];
 
-  return employeePages.some((url) => pathname.split("/").includes(url));
+  return employeePages.some((url) => pathname.split('/').includes(url));
 };
 
 const routeSubscription = (pathname) => {
-  let classname = "citizen";
+  let classname = 'citizen';
   const isEmployeeUrl = detectDsoRoute(pathname);
-  if (isEmployeeUrl && classname === "citizen") {
-    return (classname = "employee");
-  } else if (!isEmployeeUrl && classname === "employee") {
-    return (classname = "citizen");
+  if (isEmployeeUrl && classname === 'citizen') {
+    return (classname = 'employee');
+  } else if (!isEmployeeUrl && classname === 'employee') {
+    return (classname = 'citizen');
   }
 };
 
@@ -126,7 +138,19 @@ const didEmployeeHasRole = (role) => {
 const pgrAccess = () => {
   const userInfo = Digit.UserService.getUser();
   const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
-  const pgrRoles = ["PGR_LME", "PGR-ADMIN", "CSR", "CEMP", "FEMP", "DGRO", "ULB Operator", "GRO", "GO", "RO", "GA"];
+  const pgrRoles = [
+    'PGR_LME',
+    'PGR-ADMIN',
+    'CSR',
+    'CEMP',
+    'FEMP',
+    'DGRO',
+    'ULB Operator',
+    'GRO',
+    'GO',
+    'RO',
+    'GA',
+  ];
 
   const PGR_ACCESS = userRoles?.filter((role) => pgrRoles.includes(role));
 
@@ -137,16 +161,16 @@ const fsmAccess = () => {
   const userInfo = Digit.UserService.getUser();
   const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
   const fsmRoles = [
-    "FSM_CREATOR_EMP",
-    "FSM_EDITOR_EMP",
-    "FSM_VIEW_EMP",
-    "FSM_REPORT_VIEWER",
-    "FSM_DASHBOARD_VIEWER",
-    "FSM_ADMIN",
-    "FSM_DSO",
-    "FSM_DRIVER",
-    "FSM_EMP_FSTPO",
-    "FSM_COLLECTOR",
+    'FSM_CREATOR_EMP',
+    'FSM_EDITOR_EMP',
+    'FSM_VIEW_EMP',
+    'FSM_REPORT_VIEWER',
+    'FSM_DASHBOARD_VIEWER',
+    'FSM_ADMIN',
+    'FSM_DSO',
+    'FSM_DRIVER',
+    'FSM_EMP_FSTPO',
+    'FSM_COLLECTOR',
   ];
 
   const FSM_ACCESS = userRoles?.filter((role) => fsmRoles?.includes(role));
@@ -158,7 +182,7 @@ const NOCAccess = () => {
   const userInfo = Digit.UserService.getUser();
   const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
 
-  const NOC_ROLES = ["FIRE_NOC_APPROVER"];
+  const NOC_ROLES = ['FIRE_NOC_APPROVER'];
 
   const NOC_ACCESS = userRoles?.filter((role) => NOC_ROLES?.includes(role));
 
@@ -169,9 +193,11 @@ const BPAREGAccess = () => {
   const userInfo = Digit.UserService.getUser();
   const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
 
-  const BPAREG_ROLES = ["BPAREG_APPROVER", "BPAREG_DOC_VERIFIER"];
+  const BPAREG_ROLES = ['BPAREG_APPROVER', 'BPAREG_DOC_VERIFIER'];
 
-  const BPAREG_ACCESS = userRoles?.filter((role) => BPAREG_ROLES?.includes(role));
+  const BPAREG_ACCESS = userRoles?.filter((role) =>
+    BPAREG_ROLES?.includes(role)
+  );
 
   return BPAREG_ACCESS?.length > 0;
 };
@@ -181,22 +207,22 @@ const BPAAccess = () => {
   const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
 
   const BPA_ROLES = [
-    "BPA_VERIFIER",
-    "CEMP",
-    "BPA_APPROVER",
-    "BPA_FIELD_INSPECTOR",
-    "BPA_NOC_VERIFIER",
-    "AIRPORT_AUTHORITY_APPROVER",
-    "FIRE_NOC_APPROVER",
-    "NOC_DEPT_APPROVER",
-    "BPA_NOC_VERIFIER",
-    "BPA_TOWNPLANNER",
-    "BPA_ENGINEER",
-    "BPA_BUILDER",
-    "BPA_STRUCTURALENGINEER",
-    "BPA_SUPERVISOR",
-    "BPA_DOC_VERIFIER",
-    "EMPLOYEE",
+    'BPA_VERIFIER',
+    'CEMP',
+    'BPA_APPROVER',
+    'BPA_FIELD_INSPECTOR',
+    'BPA_NOC_VERIFIER',
+    'AIRPORT_AUTHORITY_APPROVER',
+    'FIRE_NOC_APPROVER',
+    'NOC_DEPT_APPROVER',
+    'BPA_NOC_VERIFIER',
+    'BPA_TOWNPLANNER',
+    'BPA_ENGINEER',
+    'BPA_BUILDER',
+    'BPA_STRUCTURALENGINEER',
+    'BPA_SUPERVISOR',
+    'BPA_DOC_VERIFIER',
+    'EMPLOYEE',
   ];
 
   const BPA_ACCESS = userRoles?.filter((role) => BPA_ROLES?.includes(role));
@@ -207,7 +233,12 @@ const BPAAccess = () => {
 const ptAccess = () => {
   const userInfo = Digit.UserService.getUser();
   const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
-  const ptRoles = ["PT_APPROVER", "PT_CEMP", "PT_DOC_VERIFIER", "PT_FIELD_INSPECTOR"];
+  const ptRoles = [
+    'PT_APPROVER',
+    'PT_CEMP',
+    'PT_DOC_VERIFIER',
+    'PT_FIELD_INSPECTOR',
+  ];
 
   const PT_ACCESS = userRoles?.filter((role) => ptRoles?.includes(role));
 
@@ -217,7 +248,12 @@ const ptAccess = () => {
 const tlAccess = () => {
   const userInfo = Digit.UserService.getUser();
   const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
-  const tlRoles = ["TL_CEMP", "TL_APPROVER", "TL_FIELD_INSPECTOR", "TL_DOC_VERIFIER"];
+  const tlRoles = [
+    'TL_CEMP',
+    'TL_APPROVER',
+    'TL_FIELD_INSPECTOR',
+    'TL_DOC_VERIFIER',
+  ];
 
   const TL_ACCESS = userRoles?.filter((role) => tlRoles?.includes(role));
 
@@ -227,9 +263,11 @@ const tlAccess = () => {
 const mCollectAccess = () => {
   const userInfo = Digit.UserService.getUser();
   const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
-  const mCollectRoles = ["UC_EMP"];
+  const mCollectRoles = ['UC_EMP'];
 
-  const MCOLLECT_ACCESS = userRoles?.filter((role) => mCollectRoles?.includes(role));
+  const MCOLLECT_ACCESS = userRoles?.filter((role) =>
+    mCollectRoles?.includes(role)
+  );
 
   return MCOLLECT_ACCESS?.length > 0;
 };
@@ -237,11 +275,13 @@ const mCollectAccess = () => {
 const receiptsAccess = () => {
   const userInfo = Digit.UserService.getUser();
   const userRoles = userInfo?.info?.roles.map((roleData) => roleData?.code);
-  const receiptsRoles = ["CR_PT"];
-  const RECEIPTS_ACCESS = userRoles?.filter((role) => receiptsRoles?.includes(role));
+  const receiptsRoles = ['CR_PT'];
+  const RECEIPTS_ACCESS = userRoles?.filter((role) =>
+    receiptsRoles?.includes(role)
+  );
   return RECEIPTS_ACCESS?.length > 0;
 };
-const hrmsRoles = ["HRMS_ADMIN"];
+const hrmsRoles = ['HRMS_ADMIN'];
 const hrmsAccess = () => {
   const userInfo = Digit.UserService.getUser();
   const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
@@ -252,7 +292,13 @@ const hrmsAccess = () => {
 const wsAccess = () => {
   const userInfo = Digit.UserService.getUser();
   const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
-  const waterRoles = ["WS_CEMP", "WS_APPROVER", "WS_FIELD_INSPECTOR", "WS_DOC_VERIFIER", "WS_CLERK"];
+  const waterRoles = [
+    'WS_CEMP',
+    'WS_APPROVER',
+    'WS_FIELD_INSPECTOR',
+    'WS_DOC_VERIFIER',
+    'WS_CLERK',
+  ];
 
   const WS_ACCESS = userRoles?.filter((role) => waterRoles?.includes(role));
 
@@ -262,7 +308,13 @@ const wsAccess = () => {
 const swAccess = () => {
   const userInfo = Digit.UserService.getUser();
   const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
-  const sewerageRoles = ["SW_CEMP", "SW_APPROVER", "SW_FIELD_INSPECTOR", "SW_DOC_VERIFIER", "SW_CLERK"];
+  const sewerageRoles = [
+    'SW_CEMP',
+    'SW_APPROVER',
+    'SW_FIELD_INSPECTOR',
+    'SW_DOC_VERIFIER',
+    'SW_CLERK',
+  ];
 
   const SW_ACCESS = userRoles?.filter((role) => sewerageRoles?.includes(role));
 
@@ -270,10 +322,17 @@ const swAccess = () => {
 };
 
 const getConfigModuleName = () => {
-  return window?.globalConfigs?.getConfig("UICONFIG_MODULENAME") || "commonUiConfig";
+  return (
+    window?.globalConfigs?.getConfig('UICONFIG_MODULENAME') || 'commonUiConfig'
+  );
+};
+
+const createFunction = (functionAsString) => {
+  return Function('return ' + functionAsString)();
 };
 
 export default {
+  createFunction,
   pdf: PDFUtil,
   downloadReceipt,
   downloadBill,
--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/molecules/CustomDropdown.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/molecules/CustomDropdown.js
@@ -1,9 +1,9 @@
-import _ from "lodash";
-import React from "react";
-import { Loader } from "../atoms/Loader";
-import RadioButtons from "../atoms/RadioButtons";
-import Dropdown from "../atoms/Dropdown";
-import MultiSelectDropdown from "../atoms/MultiSelectDropdown";
+import _ from 'lodash';
+import React from 'react';
+import { Loader } from '../atoms/Loader';
+import RadioButtons from '../atoms/RadioButtons';
+import Dropdown from '../atoms/Dropdown';
+import MultiSelectDropdown from '../atoms/MultiSelectDropdown';
 
 /**
  * Custom Dropdown / Radio Button component can be used mostly via formcomposer
@@ -63,22 +63,52 @@ or
       },
  *
  */
-const CustomDropdown = ({ t, config, inputRef, label, onChange, value, errorStyle, disable, type, additionalWrapperClass = "",mdmsv2=false,props}) => {
+const CustomDropdown = ({
+  t,
+  config,
+  inputRef,
+  label,
+  onChange,
+  value,
+  errorStyle,
+  disable,
+  type,
+  additionalWrapperClass = '',
+  mdmsv2 = false,
+  props,
+}) => {
   const master = { name: config?.mdmsConfig?.masterName };
   if (config?.mdmsConfig?.filter) {
-    master["filter"] = config?.mdmsConfig?.filter;
+    master['filter'] = config?.mdmsConfig?.filter;
   }
-  const { isLoading, data } = Digit.Hooks.useCustomMDMS(Digit.ULBService.getStateId(), config?.mdmsConfig?.moduleName, [master], {
-    select: config?.mdmsConfig?.select
-      ? Digit.Utils.createFunction(config?.mdmsConfig?.select)
-      : (data) => {
-          const optionsData = _.get(data, `${config?.mdmsConfig?.moduleName}.${config?.mdmsConfig?.masterName}`, []);
-          return optionsData
-            .filter((opt) => opt?.active)
-            .map((opt) => ({ ...opt, name: `${config?.mdmsConfig?.localePrefix}_${Digit.Utils.locale.getTransformedLocale(opt.code)}` }));
-        },
-    enabled: (config?.mdmsConfig || config?.mdmsv2) ? true : false,
-  },mdmsv2);
+  const { isLoading, data } = Digit.Hooks.useCustomMDMS(
+    Digit.ULBService.getStateId(),
+    config?.mdmsConfig?.moduleName,
+    [master],
+    {
+      select: config?.mdmsConfig?.select
+        ? Digit.Utils.createFunction(config?.mdmsConfig?.select)
+        : (data) => {
+            const optionsData = _.get(
+              data,
+              `${config?.mdmsConfig?.moduleName}.${config?.mdmsConfig?.masterName}`,
+              []
+            );
+            return optionsData
+              .filter((opt) =>
+                opt?.hasOwnProperty('active') ? opt.active : true
+              )
+              .map((opt) => ({
+                ...opt,
+                name: `${
+                  config?.mdmsConfig?.localePrefix
+                }_${Digit.Utils.locale.getTransformedLocale(opt.code)}`,
+              }));
+          },
+      enabled: config?.mdmsConfig || config?.mdmsv2 ? true : false,
+    },
+    mdmsv2
+  );
   if (isLoading) {
     return <Loader />;
   }
@@ -92,7 +122,7 @@ const CustomDropdown = ({ t, config, inputRef, label, onChange, value, errorStyl
   //   }
   //   return selectedValue
   // }
-  
+
   return (
     <React.Fragment key={config.name}>
       {/* <LabelFieldPair>
@@ -101,32 +131,37 @@ const CustomDropdown = ({ t, config, inputRef, label, onChange, value, errorStyl
           {config.required ? " * " : null}
         </CardLabel> */}
 
-      { (config.allowMultiSelect && type==="dropdown") ?
-        <div style={{ display: "grid", gridAutoFlow: "row" }}>
-        <MultiSelectDropdown
-          options={data || config?.options || []}
-          optionsKey={config?.optionsKey}
-          props={props} //these are props from Controller
-          isPropsNeeded={true}
-          onSelect={(e) => {
-            props.onChange(
-              e
-                ?.map((row) => {
-                  return row?.[1] ? row[1] : null;
-                })
-                .filter((e) => e)
-            );
-          }}
-          selected={props?.value}
-          defaultLabel={t(config?.defaultText) }
-          defaultUnit={t(config?.selectedText) || t("TQM_DROPDOWN_SELECTED")}
-          config={config}
-          
-        />
-      </div> : type === "radio" ? (
+      {config.allowMultiSelect && type === 'dropdown' ? (
+        <div style={{ display: 'grid', gridAutoFlow: 'row' }}>
+          <MultiSelectDropdown
+            options={data || config?.options || []}
+            optionsKey={config?.optionsKey}
+            props={props} //these are props from Controller
+            isPropsNeeded={true}
+            onSelect={(e) => {
+              props.onChange(
+                e
+                  ?.map((row) => {
+                    return row?.[1] ? row[1] : null;
+                  })
+                  .filter((e) => e)
+              );
+            }}
+            selected={props?.value}
+            defaultLabel={t(config?.defaultText)}
+            defaultUnit={t(config?.selectedText) || t('TQM_DROPDOWN_SELECTED')}
+            config={config}
+          />
+        </div>
+      ) : type === 'radio' ? (
         <RadioButtons
           inputRef={inputRef}
-          style={{ display: "flex", justifyContent: "flex-start", gap: "3rem", ...config.styles }}
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-start',
+            gap: '3rem',
+            ...config.styles,
+          }}
           options={data || config?.options || []}
           key={config.name}
           optionsKey={config?.optionsKey}
@@ -145,7 +180,11 @@ const CustomDropdown = ({ t, config, inputRef, label, onChange, value, errorStyl
       ) : (
         <Dropdown
           inputRef={inputRef}
-          style={{ display: "flex", justifyContent: "space-between", ...config.styles }}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            ...config.styles,
+          }}
           option={data || config?.options || []}
           key={config.name}
           optionKey={config?.optionsKey}
@@ -160,9 +199,8 @@ const CustomDropdown = ({ t, config, inputRef, label, onChange, value, errorStyl
           errorStyle={errorStyle}
           optionCardStyles={config?.optionsCustomStyle}
         />
-      )
-      } 
-      </React.Fragment>
+      )}
+    </React.Fragment>
   );
 };
 
