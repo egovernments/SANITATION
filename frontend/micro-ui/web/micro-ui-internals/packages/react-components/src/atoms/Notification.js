0e81fa6cf1c59929cac6ad61ca0f8cab27f292f6
Bhavya-egov <bhavya.mangal@egovernments.org>
2023-10-27 11:15:07 +0530

    notification API integration

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/Notification.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/Notification.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
 import Header from "./Header";
 import Button from "./Button";
+import { useHistory } from "react-router-dom";
 import { CloseSvg, NotificationBell } from "./svgindex";
 
 
 const Notification = ({ actions }) => {
     const { t } = useTranslation();
-
+    const history = useHistory();
     const [notifications, setNotifications] = useState([]);
 
     useEffect(() => {
@@ -23,12 +24,13 @@ const Notification = ({ actions }) => {
         updatedNotifications.splice(index, 1);
         setNotifications(updatedNotifications);
     };
+
     return (
         <div className="Container">
             <div className="headerContainer">
                 <NotificationBell height={25} width={25} fill="#F47738" className={"bell-icon"} />
                 <Header >{t("ES_TQM_NOTIFICATIONS")}</Header>
-                <a href="#" className="clear-all-link" onClick={handleClearAll}>{t("TQM_CLEAR_SEARCH")}</a>
+                <div className="clear-all-link" onClick={handleClearAll}>{t("TQM_CLEAR_SEARCH")}</div>
             </div>
             {notifications.length > 0 &&
                 <div className="NotificationItem">
@@ -43,10 +45,15 @@ const Notification = ({ actions }) => {
                                 <div className="notificationContent">
                                     <div>
                                         <p>{t(item.eventNotificationText)}</p>
-                                        <p>{item.timePastAfterEventCreation + ` ${t(item.timeApproxiamationInUnits)}`}</p>
+                                        <p>{item.timePastAfterEventCreation }</p>
                                     </div>
                                     <div className="button-container">
-                                        <Button className={"header-btn viewDetailsButton"} label={t("ES_TQM_VIEW_TEST_DETAILS")} variation="secondary" type="button" onButtonClick={() => { }} />
+                                        <Button className={"header-btn viewDetailsButton"} label={t("TQM_VIEW_TEST_DETAILS")} variation="secondary" type="button" 
+                                        onButtonClick={() => {
+                                            const url = item.actionUrl;
+                                            const id = url.split('id=')[1];
+                                            history.push(`view-test-results?id=${id}`)
+                                        }} />
                                     </div>
                                 </div>
                             </div>
