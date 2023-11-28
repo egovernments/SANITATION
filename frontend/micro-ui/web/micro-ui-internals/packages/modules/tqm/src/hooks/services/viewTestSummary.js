54052635751c33a26db51ec51e08918275d38528
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-11-28 12:31:01 +0530

    SM-3497/SM-3486

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tqm/src/hooks/services/viewTestSummary.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tqm/src/hooks/services/viewTestSummary.js
@@ -23,19 +23,27 @@ export const viewTestSummary = async ({ t, id }) => {
       },
       {
         key: "ES_TQM_LABEL_PLANT_NAME",
-        value: t(Digit.Utils.locale.getTransformedLocale(`PQM.Plant_${testResponse?.plantCode}`)) || "N/A",
+        value: testResponse?.plantCode ? t(Digit.Utils.locale.getTransformedLocale(`PQM.Plant_${testResponse?.plantCode}`)) : "N/A",
       },
       {
         key: "ES_TQM_LABEL_TREATMENT_PROCESS",
-        value: t(Digit.Utils.locale.getTransformedLocale(`PQM.Process_${testResponse?.processCode}`)) || "N/A",
+        value: testResponse?.processCode ? t(Digit.Utils.locale.getTransformedLocale(`PQM.Process_${testResponse?.processCode}`)) : "N/A",
       },
       {
         key: "ES_TQM_LABEL_STAGE",
-        value: t(Digit.Utils.locale.getTransformedLocale(`PQM.Stage_${testResponse?.stageCode}`)) || "N/A",
+        value: testResponse?.stageCode ? t(Digit.Utils.locale.getTransformedLocale(`PQM.Stage_${testResponse?.stageCode}`)) : "N/A",
       },
       {
         key: "ES_TQM_LABEL_TEST_TYPE",
-        value: t(Digit.Utils.locale.getTransformedLocale(`PQM.TestType_${testResponse?.testType}`)) || "N/A",
+        value: testResponse?.testType ? t(Digit.Utils.locale.getTransformedLocale(`PQM.TestType_${testResponse?.testType}`)) : "N/A",
+      },
+      {
+        key: t("ES_TQM_LABEL_OUTPUT_TYPE"),
+        value: testResponse?.materialCode ? t(Digit.Utils.locale.getTransformedLocale(`PQM.Material_${testResponse?.materialCode}`)) : "N/A",
+      },
+      {
+        key: t("ES_TQM_LABEL_LAB_NAME"),
+        value: testResponse?.labAssignedTo ? t(Digit.Utils.locale.getTransformedLocale(`PQM.QualityTestLab_${testResponse?.labAssignedTo}`)) : "N/A",
       },
       {
         key: "ES_TQM_LABEL_TEST_SCHEDULED_ON",
