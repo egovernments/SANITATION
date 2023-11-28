eec16ac8d9caec407468c6c8fc5981aea25b9db2
Nipun Arora <aroranipun1@gmail.com>
2023-11-28 12:00:07 +0530

    Reverted Changes

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/pages/employee/ApplicationDetails/config/AcceptDso.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/pages/employee/ApplicationDetails/config/AcceptDso.js
@@ -1,23 +1,7 @@
 import React from "react";
 import { CardLabelError, Dropdown } from "@egovernments/digit-ui-react-components";
 
-export const configAcceptDso = ({
-  t,
-  dsoData,
-  dso,
-  selectVehicleNo,
-  vehicleNoList,
-  vehicleNo,
-  vehicle,
-  noOfTrips,
-  vehicleDriverList,
-  vehicleDriver,
-  selectVehicleDriver,
-  workerList,
-  worker,
-  selectWorker,
-  action,
-}) => {
+export const configAcceptDso = ({ t, dsoData, dso, selectVehicleNo, vehicleNoList, vehicleNo, vehicle, noOfTrips, action }) => {
   return {
     label: {
       heading: `ES_FSM_ACTION_TITLE_${action}`,
@@ -33,7 +17,9 @@ export const configAcceptDso = ({
             type: "dropdown",
             populators: (
               <React.Fragment>
-                {!vehicleNoList?.length ? <CardLabelError>{t("ES_FSM_NO_VEHICLE_AVAILABLE")}</CardLabelError> : null}
+                {!vehicleNoList?.length ? (
+                  <CardLabelError>{t("ES_FSM_NO_VEHICLE_AVAILABLE")}</CardLabelError>
+                ) : null}
                 <Dropdown
                   option={vehicleNoList}
                   autoComplete="off"
@@ -59,44 +45,6 @@ export const configAcceptDso = ({
             disable: true,
           },
           {
-            label: t("ES_FSM_DRIVER"),
-            isMandatory: true,
-            type: "dropdown",
-            populators: (
-              <React.Fragment>
-                {!vehicleDriverList?.length ? <CardLabelError>{t("ES_FSM_NO_DRIVER_AVAILABLE")}</CardLabelError> : null}
-                <Dropdown
-                  option={vehicleDriverList}
-                  autoComplete="off"
-                  optionKey="name"
-                  id="driver"
-                  select={selectVehicleDriver}
-                  selected={vehicleDriver}
-                  disable={vehicleDriverList?.length > 0 ? false : true}
-                />
-              </React.Fragment>
-            ),
-          },
-          {
-            label: t("ES_FSM_ACTION_SANITATION_WORKER"),
-            isMandatory: true,
-            type: "dropdown",
-            populators: (
-              <React.Fragment>
-                {!workerList?.length ? <CardLabelError>{t("ES_FSM_NO_DRIVER_AVAILABLE")}</CardLabelError> : null}
-                <MultiSelectDropdown
-                  options={workerList}
-                  autoComplete="off"
-                  optionsKey="name"
-                  id="driver"
-                  onSelect={selectWorker}
-                  selected={worker}
-                  disable={workerList?.length > 0 ? false : true}
-                />
-              </React.Fragment>
-            ),
-          },
-          {
             label: t("ES_FSM_ACTION_NUMBER_OF_TRIPS"),
             isMandatory: true,
             type: "text",
@@ -105,7 +53,7 @@ export const configAcceptDso = ({
               validation: {
                 required: true,
               },
-              defaultValue: noOfTrips,
+              defaultValue: noOfTrips
             },
             disable: true,
           },
