42e30ab7ad447ba3fb39787865553d5155e0c0e8
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-12-01 11:34:20 +0530

    SM-3488 UI issues

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/molecules/WorkflowStatusFilter.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/molecules/WorkflowStatusFilter.js
@@ -1,39 +1,41 @@
-import React, { Fragment,useEffect,useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import CheckBox from "../atoms/CheckBox";
 import { Loader } from "../atoms/Loader";
 import CardLabel from "../atoms/CardLabel";
 
-const WorkflowStatusFilter = ({ props, t, populators, formData,inboxResponse }) => {
-//from inbox response get the statusMap and show the relevant statuses
+const WorkflowStatusFilter = ({ props, t, populators, formData, inboxResponse }) => {
+  //from inbox response get the statusMap and show the relevant statuses
   //here need to filter these options based on logged in user(and test based on single roles in every inbox)(new requirement from vasanth)
 
-  
-  const [statusMap,setStatusMap] = useState(null)
+  const [statusMap, setStatusMap] = useState(null);
 
   useEffect(() => {
-    if(inboxResponse) {
-      setStatusMap(inboxResponse.statusMap?.map(row => {
-       return {
-         uuid:row.statusid,
-         state: row.state || row.applicationstatus,
-         businessService:row?.businessservice
-       }
-      }))
+    if (inboxResponse) {
+      setStatusMap(
+        inboxResponse.statusMap?.map((row) => {
+          return {
+            uuid: row.statusid,
+            state: row.state || row.applicationstatus,
+            businessService: row?.businessservice,
+            count: row?.count,
+          };
+        })
+      );
     }
-  }, [inboxResponse])
-  
+  }, [inboxResponse]);
 
   if (!statusMap && !inboxResponse) return <Loader />;
 
+  console.log("inboxResponse", inboxResponse);
   return (
     <>
-
-{ statusMap&&statusMap.length>0&&populators?.componentLabel && (
-                  <CardLabel style={{...props.labelStyle,marginBottom:"0.4rem"}}>
-                    {t(populators?.componentLabel)}{ populators?.isMandatory ? " * " : null }
-                  </CardLabel>) 
-                }
-        {statusMap?.map((row) => {
+      {statusMap && statusMap.length > 0 && populators?.componentLabel && (
+        <CardLabel style={{ ...props.labelStyle, marginBottom: "0.4rem" }}>
+          {t(populators?.componentLabel)}
+          {populators?.isMandatory ? " * " : null}
+        </CardLabel>
+      )}
+      {statusMap?.map((row) => {
         return (
           <CheckBox
             onChange={(e) => {
@@ -45,12 +47,12 @@ const WorkflowStatusFilter = ({ props, t, populators, formData,inboxResponse }) 
             }}
             value={row.uuid}
             checked={formData?.[populators.name]?.[row.uuid]}
-            label={t(Digit.Utils.locale.getTransformedLocale(`${populators.labelPrefix}${row?.businessService}_STATE_${row?.state}`))}
+            label={`${t(Digit.Utils.locale.getTransformedLocale(`${populators.labelPrefix}${row?.businessService}_STATE_${row?.state}`))} (${row.count})`}
           />
         );
       })}
     </>
-  )
+  );
 };
 
 export default WorkflowStatusFilter;
