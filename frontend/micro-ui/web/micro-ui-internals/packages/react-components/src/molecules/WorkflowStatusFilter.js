424fbd5785e37f5bea8d83185b43d067c55dc981
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-12-01 11:36:51 +0530

    SM-3488 UI issues

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/molecules/WorkflowStatusFilter.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/molecules/WorkflowStatusFilter.js
@@ -26,7 +26,6 @@ const WorkflowStatusFilter = ({ props, t, populators, formData, inboxResponse })
 
   if (!statusMap && !inboxResponse) return <Loader />;
 
-  console.log("inboxResponse", inboxResponse);
   return (
     <>
       {statusMap && statusMap.length > 0 && populators?.componentLabel && (
@@ -47,7 +46,7 @@ const WorkflowStatusFilter = ({ props, t, populators, formData, inboxResponse })
             }}
             value={row.uuid}
             checked={formData?.[populators.name]?.[row.uuid]}
-            label={`${t(Digit.Utils.locale.getTransformedLocale(`${populators.labelPrefix}${row?.businessService}_STATE_${row?.state}`))} (${row.count})`}
+            label={`${t(Digit.Utils.locale.getTransformedLocale(`${populators.labelPrefix}${row?.businessService}_STATE_${row?.state}`))} (${row?.count || 0})`}
           />
         );
       })}
