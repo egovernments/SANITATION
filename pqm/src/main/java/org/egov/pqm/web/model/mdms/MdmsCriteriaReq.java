1aeb9f2a71fba12836d2287b5860f5d1f2ad8fe1
Priyanka-eGov <priyanka@egovernments.org>
2023-12-19 14:21:46 +0530

    Adding @JsonIgnoreProperties annotation for Mdms POJOs

--- pqm/src/main/java/org/egov/pqm/web/model/mdms/MdmsCriteriaReq.java
+++ pqm/src/main/java/org/egov/pqm/web/model/mdms/MdmsCriteriaReq.java
@@ -1,5 +1,6 @@
 package org.egov.pqm.web.model.mdms;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.egov.common.contract.request.RequestInfo;
@@ -12,6 +13,7 @@ import javax.validation.Valid;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MdmsCriteriaReq {
   @JsonProperty("RequestInfo")
   @Valid
