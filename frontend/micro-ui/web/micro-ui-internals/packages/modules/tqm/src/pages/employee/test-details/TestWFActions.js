033986ddfa74d765b059a120ef370126f3cf36e9
nabeelmd-eGov <94039229+nabeelmd-eGov@users.noreply.github.com>
2024-01-23 14:00:55 +0530

    Sm 2151 (#442)
    * DPG-2151: tqm responsiveness
    
    * DPG-2151: tqm resp
    
    ---------
    
    Co-authored-by: nabeelmd-eGov <nabeel.md@egovernments.com>

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tqm/src/pages/employee/test-details/TestWFActions.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tqm/src/pages/employee/test-details/TestWFActions.js
@@ -82,7 +82,7 @@ function TestWFActions({ id, t, WFData, actionData, actionState, submitAction, t
         onSubmit={onSubmit}
         label={t(actionState === "SCHEDULED" ? "ES_TQM_UPDATE_STATUS_BUTTON" : "ES_TQM_SUBMIT_TEST_RESULTS_BUTTON")}
         submitInForm={isMobile ? true : false}
-        cardClassName={"testwf"}
+        cardClassName={isMobile ? "testwf" : "employeeCard-override"}
       />
       {showPopUp && (
         <Modal
