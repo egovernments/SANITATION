bf9b22f747e8771194df5b68618fc3472174d546
nabeelmd-eGov <nabeel.md@egovernments.com>
2024-01-29 11:33:46 +0530

    DPG-2038: Added comments for scroll persistent

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/useScrollPersistence.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/useScrollPersistence.js
@@ -1,3 +1,48 @@
+/*
+useScrollPersistence Hook
+  This custom React Hook, useScrollPersistence, is designed to store and retrieve scroll positions based on the current URL. It utilizes the browser's localStorage to persistently store the scroll position when navigating between pages. Additionally, it handles scroll position restoration after a page reload.
+  useScrollPersistence Hook
+
+  This custom React Hook, useScrollPersistence, is designed to manage and persist the scroll position of a page based on its URL. It includes functionality to store the scroll position in the browser's localStorage and restore it when the user navigates between pages or refreshes the page.
+
+  How it Works:
+
+  - The hook uses the current URL as a key to store and retrieve scroll positions in localStorage.
+  - On component mount, it retrieves the stored scroll position and sets it as the initial state.
+  - Listens for scroll events and updates the scroll position state accordingly.
+  - Utilizes localStorage to prevent unintended function calls during page reloads.
+  - Handles scroll position restoration after a page reload.
+
+  Methods:
+  - `getUrlKey`: Generates a key based on the current URL for storing scroll positions.
+  - `initialScrollPosition`: Retrieves the initial scroll position from localStorage.
+  - `handleScroll`: Listens for scroll events and updates the scroll position state and localStorage.
+  - `handleBeforeUnload`: Stores scroll position before a page unload to ensure proper restoration.
+  
+  How to Use:
+  1. Call the hook in your functional component:
+      const scrollPosition = Digit.Hooks.useScrollPersistence();
+
+  2. The hook returns the current scroll position, which you can use in your component.
+
+  3. To scroll to the persisted position when scroll persistence is enabled, use the following useEffect in the desired file:
+      useEffect(() => {
+      window.scrollTo(0, scrollPosition);
+      }, [scrollPosition]);
+  
+  4. To destroy the stored scroll value, use the following use effect in the desired file where you want to remove:
+      useEffect(() =>{
+      localStorage.removeItem(URL_KEY);
+      }, [])
+  
+  NOTE: 
+    In mobile view, clicking on back will re run scroll function causing reset the scroll value to 0. So add to the "scrollValue" to enable a prevented check:  
+      useEffect(() => {
+        const checkValue = localStorage.getItem(URL_KEY);
+        localStorage.setItem("scrollValue", checkValue);
+      }, []); 
+*/
+
 import { useEffect, useState } from "react";
 import throttle from "lodash/throttle";
 
@@ -26,7 +71,7 @@ const useScrollPersistence = () => {
         localStorage.removeItem("scrollValue");
         return;
       }
-      // Check to prevent unintended function calls 
+      // Check to prevent unintended function calls
       if (checkReloadValue > 0 && isScrolling) {
         localStorage.removeItem("reloadScrollValue");
         return;
