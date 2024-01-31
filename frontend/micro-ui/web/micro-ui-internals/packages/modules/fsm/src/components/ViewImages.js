d57091211b608b9e014c3655e16b3ba6e6c241d4
nabeelmd-eGov <nabeel.md@egovernments.com>
2024-01-31 17:53:02 +0530

    SM-3717: SW vendor details screen change

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/components/ViewImages.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/fsm/src/components/ViewImages.js
@@ -51,7 +51,7 @@ export const ViewImages = (props) => {
           <React.Fragment key={index}>
             <a target="_" href={doc.fullImage} style={{ minWidth: "80px", marginRight: "10px", maxWidth: "100px", height: "auto" }} key={index}>
               <DocumentRect />
-              <p className="tqm-document-title">t("ES_FSM_DOCUMENT")</p>
+              <p className="tqm-document-title">{t("ES_FSM_DOCUMENT")}</p>
             </a>
           </React.Fragment>
         ))}
