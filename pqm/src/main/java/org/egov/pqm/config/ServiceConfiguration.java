d0fb5aed5c7738dfda53fad33b9a8d15cc5a0543
SivajiGanesh Nangireddy <sivaji.ganesh@egovernments.org>
2024-02-14 13:00:37 +0530

    SM-3900: update test documents on save as draft and update results

--- pqm/src/main/java/org/egov/pqm/config/ServiceConfiguration.java
+++ pqm/src/main/java/org/egov/pqm/config/ServiceConfiguration.java
@@ -1,17 +1,14 @@
 package org.egov.pqm.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
@@ -78,6 +75,9 @@ public class ServiceConfiguration {
   @Value("${egov.plant.user.update.kafka.topic}")
   private String plantUserUpdateTopic;
 
+  @Value("${egov.test.document.update.kafka.topic}")
+  private String updateTestDocumentsTopic;
+
   //workflow
   @Value("${create.pqm.workflow.name}")
   private String businessServiceValue;
@@ -133,5 +133,7 @@ public class ServiceConfiguration {
   //tenantId
   @Value("${egov.statelevel.tenantid}")
   private String egovStateLevelTenantId;
+
+
 
 }
