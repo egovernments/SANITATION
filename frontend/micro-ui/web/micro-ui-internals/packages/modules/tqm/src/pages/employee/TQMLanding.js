baaf20b457e7fd7d90f5bb16439523754a75cc5f
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2024-03-19 15:37:55 +0530

    SM-3989: Hide inbox,Vehicle log,View dashboard module

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/tqm/src/pages/employee/TQMLanding.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/tqm/src/pages/employee/TQMLanding.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 import {
   BackButton,
   Card,
@@ -13,10 +13,10 @@ import {
   IssueIcon,
   TreatmentQualityIcon,
   VehicleLogIcon,
-} from "@egovernments/digit-ui-react-components";
-import { useTranslation } from "react-i18next";
-import { useHistory } from "react-router-dom";
-import TQMPendingTask from "./TQMPendingTask";
+} from '@egovernments/digit-ui-react-components';
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
+import TQMPendingTask from './TQMPendingTask';
 
 const TQMLanding = () => {
   const { t } = useTranslation();
@@ -26,24 +26,9 @@ const TQMLanding = () => {
   const history = useHistory();
   const module = [
     {
-      name: "ES_COMMON_INBOX",
-      link: `/${window?.contextPath}/employee/fsm/fstp-inbox`,
-      icon: <InboxIcon />,
-    },
-    {
-      name: "ES_FSM_VEHICLE_LOG",
-      link: `/${window?.contextPath}/employee/fsm/fstp-add-vehicle`,
-      icon: <VehicleLogIcon />,
-    },
-    {
-      name: "ES_TQM_TREATMENT_QUALITY",
+      name: 'ES_TQM_TREATMENT_QUALITY',
       link: `/${window?.contextPath}/employee/tqm/home`,
       icon: <TreatmentQualityIcon />,
-    },
-    {
-      name: "ES_TQM_VIEW_DASHBOARD",
-      link: `/${window?.contextPath}/employee/dss/dashboard/pqm`,
-      icon: <DashboardIcon />,
     },
   ];
 
