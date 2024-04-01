a47fdf88add43d255158890fcab69fdd94a8efbc
JithendarKumar-eGov <jithendar.k@egovernments.org>
2024-04-01 17:10:10 +0530

    Added jsonpath

--- pqm/src/main/java/org/egov/pqm/web/model/EgovPdfResp.java
+++ pqm/src/main/java/org/egov/pqm/web/model/EgovPdfResp.java
@@ -4,15 +4,19 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EgovPdfResp {
 	
 	@JsonProperty("filestoreIds")
