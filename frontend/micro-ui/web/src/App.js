72e36b80ba4a95edf58e27022c72e47063e4e2b6
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2024-05-16 12:56:22 +0530

    Updated modules to handle only TQM

--- frontend/micro-ui/web/src/App.js
+++ frontend/micro-ui/web/src/App.js
@@ -1,27 +1,34 @@
-import React from "react";
+import React from 'react';
 
-import { initDSSComponents } from "@egovernments/digit-ui-module-dss";
-import { DigitUI,initCoreComponents } from "@egovernments/digit-ui-module-core";
-import { initLibraries } from "@egovernments/digit-ui-libraries";
-import { initEngagementComponents } from "@egovernments/digit-ui-module-engagement";
-import { initFSMLibraries } from "@egovernments/digit-ui-fsm-libraries";
+import { initDSSComponents } from '@egovernments/digit-ui-module-dss';
+import {
+  DigitUI,
+  initCoreComponents,
+} from '@egovernments/digit-ui-module-core';
+import { initLibraries } from '@egovernments/digit-ui-libraries';
+import { initEngagementComponents } from '@egovernments/digit-ui-module-engagement';
+import { initFSMLibraries } from '@egovernments/digit-ui-fsm-libraries';
 
-import { initFSMComponents } from "@egovernments/digit-ui-module-fsm";
-import { initTQMComponents } from "@egovernments/digit-ui-module-tqm";
-import { initHRMSComponents } from "@egovernments/digit-ui-module-hrms";
-import { PaymentModule, PaymentLinks, paymentConfigs } from "@egovernments/digit-ui-module-common";
-import { initUtilitiesComponents } from "@egovernments/digit-ui-module-utilities";
-import { UICustomizations } from "./Customisations/UICustomizations";
+import { initFSMComponents } from '@egovernments/digit-ui-module-fsm';
+import { initTQMComponents } from '@egovernments/digit-ui-module-tqm';
+import { initHRMSComponents } from '@egovernments/digit-ui-module-hrms';
+import {
+  PaymentModule,
+  PaymentLinks,
+  paymentConfigs,
+} from '@egovernments/digit-ui-module-common';
+import { initUtilitiesComponents } from '@egovernments/digit-ui-module-utilities';
+import { UICustomizations } from './Customisations/UICustomizations';
 
-window.contextPath = window?.globalConfigs?.getConfig("CONTEXT_PATH");
+window.contextPath = window?.globalConfigs?.getConfig('CONTEXT_PATH');
 window.Digit.Customizations = {
-  commonUiConfig: UICustomizations
+  commonUiConfig: UICustomizations,
 };
 
 initLibraries();
 initFSMLibraries();
 
-const enabledModules = ["Payment", "DSS", "Engagement", "HRMS","Tqm","Utilities"];
+const enabledModules = ['Tqm'];
 window.Digit.ComponentRegistryService.setupRegistry({
   ...paymentConfigs,
   PaymentModule,
@@ -34,21 +41,27 @@ initFSMComponents();
 initHRMSComponents();
 initUtilitiesComponents();
 
-
 const moduleReducers = (initData) => ({
   initData,
 });
 
-
 //calling it here so that UICustomizations inside tqm gets added after the common Customizations are added
 initTQMComponents();
 function App() {
-  window.contextPath = window?.globalConfigs?.getConfig("CONTEXT_PATH");
-  const stateCode = window.globalConfigs?.getConfig("STATE_LEVEL_TENANT_ID") || process.env.REACT_APP_STATE_LEVEL_TENANT_ID;
+  window.contextPath = window?.globalConfigs?.getConfig('CONTEXT_PATH');
+  const stateCode =
+    window.globalConfigs?.getConfig('STATE_LEVEL_TENANT_ID') ||
+    process.env.REACT_APP_STATE_LEVEL_TENANT_ID;
   if (!stateCode) {
     return <h1>stateCode is not defined</h1>;
   }
-  return <DigitUI stateCode={stateCode} enabledModules={enabledModules} moduleReducers={moduleReducers} />;
+  return (
+    <DigitUI
+      stateCode={stateCode}
+      enabledModules={enabledModules}
+      moduleReducers={moduleReducers}
+    />
+  );
 }
 
 export default App;
