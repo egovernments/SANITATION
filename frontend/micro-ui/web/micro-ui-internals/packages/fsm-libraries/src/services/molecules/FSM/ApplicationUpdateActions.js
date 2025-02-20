bf96001e513dab16eb9cd061bc21f70d05d4299e
Hari-eGov <hariprasad@egovernments.org>
2025-02-20 14:32:03 +0530

    tenantId updated

--- frontend/micro-ui/web/micro-ui-internals/packages/fsm-libraries/src/services/molecules/FSM/ApplicationUpdateActions.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/fsm-libraries/src/services/molecules/FSM/ApplicationUpdateActions.js
@@ -3,8 +3,6 @@ import { FSMService } from "../../elements/FSM";
 const ApplicationUpdateActions = async (applicationData, tenantId) => {
   try {
     const response = await FSMService.update(applicationData, tenantId);
-    console.log(`*** LOG RES2***`,response);
-
     return response;
   } catch (error) {
     throw new Error(error?.response?.data?.Errors[0].message);
