303c08ec34e15feec8b659594fe6d8928a0c8b6d
JithendarKumar-eGov <jithendar.k@egovernments.org>
2025-03-07 13:26:41 +0530

    Directlry converted to bigdecimal

--- fsm-calculator/src/main/java/org/egov/fsm/calculator/services/CalculationService.java
+++ fsm-calculator/src/main/java/org/egov/fsm/calculator/services/CalculationService.java
@@ -311,7 +311,7 @@ public class CalculationService {
 				: new HashMap<>();
 		if (oldAdditionalDetails != null
 				|| oldAdditionalDetails != null && oldAdditionalDetails.get("tripAmount") != null) {
-			amount = BigDecimal.valueOf(Double.valueOf(String.valueOf(oldAdditionalDetails.get("tripAmount"))));
+			amount = new BigDecimal(String.valueOf(oldAdditionalDetails.get("tripAmount")));
 
 			// fetch advance amount from fsm application
 		} else {
