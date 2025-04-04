3e6cbb8c0ef9476afce6e1ba3f86903cfc76aab1
JithendarKumar-eGov <jithendar.k@egovernments.org>
2025-04-04 12:12:32 +0530

    #CCSD-55 & CCSD-56 modified migrate

--- fsm/src/main/resources/db/migrate.sh
+++ fsm/src/main/resources/db/migrate.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
-
-#!/bin/sh
 baseurl=$DB_URL
 echo "the baseurl : $DB_URL"
 schemasetter="?currentSchema="
 schemas=$SCHEMA_NAME
 echo "the schemas : $schemas"
-for schemaname in ${schemas//,/ }
+
+# Use `tr` to replace commas with spaces for portability in `sh`
+for schemaname in $(echo "$schemas" | tr ',' ' ')
 do
     echo "the schema name : ${baseurl}${schemasetter}${schemaname}"
-    flyway -url=${baseurl}${schemasetter}${schemaname}  -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true -outOfOrder=true -ignoreMissingMigrations=true migrate
+    flyway -url="${baseurl}${schemasetter}${schemaname}" \
+           -table="$SCHEMA_TABLE" \
+           -user="$FLYWAY_USER" \
+           -password="$FLYWAY_PASSWORD" \
+           -locations="$FLYWAY_LOCATIONS" \
+           -baselineOnMigrate=true \
+           -outOfOrder=true \
+           migrate
 done
